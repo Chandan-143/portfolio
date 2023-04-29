8b42f24e63f6722987c043227089c7de4ffe5051
chandan kumar <chandankumarmech18@gmail.com>
2023-04-29 19:27:45 +0530

    portfolio

---
+++ .vscode/script.js
@@ -0,0 +1,112 @@
+
+
+let menuIcon=document.querySelector('#menu-icon');
+let navbar=document.querySelector(".navbar");
+
+menuIcon.onclick=()=>{
+  menuIcon.classList.toggle('bx-x');
+  navbar.classList.toggle('action');
+ 
+};
+
+
+
+let sections=document.querySelectorAll('section');
+// menu icon navbar
+let navLink=document.querySelectorAll('header nav a');
+
+
+// scroll section active link
+
+let section=document.querySelectorAll('section')
+let navLinks=document.querySelectorAll('header nav a')
+
+
+window.onscroll = () => {
+  section.forEach(sec=>{
+    let top=window.scrollY;
+    let offset=sec.offsetTop-150;
+    let height=sec.offsetHeight;
+    let id=sec.getAttribute('id');
+
+    if(top>=offset && top <offset+height){
+      navLink.forEach(links=>{
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id + ']').classList.add('active');
+      })
+    }
+  });
+  
+
+  // sticky navbar
+
+  let header = document.querySelector('.header');
+  
+  header.classList.toggle('sticky', window.scrollY > 100);
+
+
+  // remove menu icon navbar when click navbar
+
+  menuIcon.classList.remove('bx-x');
+  navbar.classList.remove('action');
+ 
+
+};
+
+//swaper
+
+var swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1,
+    spaceBetween: 50,
+    loop: true,
+    grabCursor:true,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
+
+  sections.forEach(sec => {
+    let top=window.scrollY;
+    let offset= sec.offsetTop-150;
+    let height=sec.offsetHeight;
+    let id=sec.getAttribute('id');
+
+    if(top>=offset && top<offset+height){
+      navLink.forEach(links=>{
+        links.classList.remove('active');
+        document.querySelector('header nav a[href*=' + id+']').classList.add('active');
+      });
+    };
+  });
+
+
+
+  // dark light mood
+
+  let darkmodeicon=document.querySelector("#darkmood-icon");
+
+  darkmodeicon.onclick=()=>{
+    darkmodeicon.classList.toggle('bxs-sun');
+    document.body.classList.toggle('dark-mode');
+
+  };
+
+  ScrollReveal({
+     reset: true,
+    distance:'80px',
+    duration:2000,
+    delay:200
+  });
+
+ScrollReveal().reveal('.home-content, .heading', { origin: 'top' });
+
+
+
+
+ 
+
